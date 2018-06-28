79e07cd7ee0a8714019d66de6c49bdf77006a3a9
Lukas Alber <36759141+luksen99@users.noreply.github.com>
2018-06-28 16:43:53 +0200

    Update structure.py

--- structure.py
+++ structure.py
@@ -4,7 +4,7 @@
 
 
 # This file is part of the udkm1Dsimpy module.
-#
+#45
 # udkm1Dsimpy is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
